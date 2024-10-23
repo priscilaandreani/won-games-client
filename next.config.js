4b20271181637aba17eae0c4ad499865a7c1359f
priscilaandreani <priscilaandreani@gmail.com>
2024-10-22 22:10:19 -0300

    refactor: update next.config.js
    Refactor next.config.js to use TypeScript type checking and update the configuration options.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   compiler: {
     styledComponents: true
   }
+  /* config options here */
 };
+
+export default nextConfig;
