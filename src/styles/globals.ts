a6120545d07e0e15391b8430ebdca07023c47056
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-11-06 22:39:15 -0300

    feat: integrate styled-components theme provider and update global styles

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,6 +1,6 @@
 'use client';
 
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 
@@ -38,17 +38,17 @@ const GlobalStyles = createGlobalStyle`
     --moz-osx-font-smoothing: grayscale
   }
 
-  html {
-    font-size: 62,5%; // 1rem = 10px
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: 62, 5%; // 1rem = 10px
+    }
 
-  html, border-style, #__next {
-    height: 100%;
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
 
-  body {
-    font-family: 'Poppins', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
 
 
 `;
